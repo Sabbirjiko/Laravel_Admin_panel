88eb969b7c89ff0e3fa6133d3881d6c4058afef8
Sheikh Md Sabbir Hossain <sabbirjoy8@gmail.com>
2019-11-13 22:05:51 +0600

    Templates
    Templates

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('/admin','AdminController@login')->name('admin_login');
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {
+	
+	Route::get('/admin/dashboard', 'AdminController@dashboard')->name('admin_dashboard');
+	Route::get('/admin/settings', 'AdminController@settings')->name('admin_settings');
+	Route::get('/admin/check_pass', 'AdminController@check_pass')->name('admin_check_pass');
+	Route::any('/admin/update_pass', 'AdminController@update_pass')->name('admin.update_pass');
+
+});
+
+
+Route::get('/logout', 'AdminController@logout')->name('admin_logout');
+Route::get('/home', 'HomeController@index')->name('home');
+
